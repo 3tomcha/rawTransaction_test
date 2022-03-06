5c13d4fb28d522f922fe0d5458761f5f717f73e2
Tomoya Kobayashi <tomocha.marika@gmail.com>
2022-03-06 10:41:31 +0900

    getTransactionCount

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import Web3 from 'web3/dist/web3.min.js'
-const provider = new Web3.providers.HttpProvider(`https://rinkeby.infura.io/v3/${import.meta.VITE_APP_INFURA_KEY}`);
+const provider = new Web3.providers.HttpProvider(`https://rinkeby.infura.io/v3/${import.meta.env.VITE_APP_INFURA_KEY}`);
 const web3 = new Web3(provider);
-console.log(web3);
+const sender = "0x32a9E70324862ef7BF8bA7610AF701822ddE5364";
+const transactionCount = await web3.eth.getTransactionCount(sender);
+alert(transactionCount);
 </script>
 
 <template>

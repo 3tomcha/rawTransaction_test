046be583e76bb47321d3dfb6a4eefd0fa6c4127a
Tomoya Kobayashi <tomocha.marika@gmail.com>
2022-03-06 11:39:36 +0900

    add node support

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,39 @@
 import { fileURLToPath, URL } from 'url'
-
+import NodeGlobalsPolyfillPlugin from '@esbuild-plugins/node-globals-polyfill'
+import NodeModulesPolyfillPlugin from '@esbuild-plugins/node-modules-polyfill'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import rollupNodePolyFill from 'rollup-plugin-polyfill-node'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      zlib: "rollup-plugin-polyfill-node/polyfills/zlib",
+      stream: "rollup-plugin-polyfill-node/polyfills/stream",
+    }
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: "globalThis"
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true
+        }),
+        NodeModulesPolyfillPlugin()
+      ]
+    }
+  },
+  build: {
+    rollupOptions: {
+      plugins: [
+        rollupNodePolyFill()
+      ]
     }
   }
 })

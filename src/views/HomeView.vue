297f6bfff23335f7227b8c0ef1599e99946e194a
Tomoya Kobayashi <tomocha.marika@gmail.com>
2022-03-06 10:26:58 +0900

    import web3

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
+import Web3 from 'web3/dist/web3.min.js'
+const provider = new Web3.providers.HttpProvider(`https://rinkeby.infura.io/v3/${import.meta.VITE_APP_INFURA_KEY}`);
+const web3 = new Web3(provider);
+console.log(web3);
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    aaa
   </main>
 </template>
---
+++ src/web3.d.ts
@@ -0,0 +1,5 @@
+// src/web3.d.ts
+declare module 'web3/dist/web3.min.js' {
+    import Web3 from 'web3'
+    export default Web3
+  }

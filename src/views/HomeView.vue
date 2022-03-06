fca2a00461cd137d095e16753aa52de8367fd130
Tomoya Kobayashi <tomocha.marika@gmail.com>
2022-03-06 10:55:39 +0900

    get new Nnonce

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,8 @@ const provider = new Web3.providers.HttpProvider(`https://rinkeby.infura.io/v3/$
 const web3 = new Web3(provider);
 const sender = "0x32a9E70324862ef7BF8bA7610AF701822ddE5364";
 const transactionCount = await web3.eth.getTransactionCount(sender);
-alert(transactionCount);
+const newNonce = web3.utils.toHex(transactionCount);
+alert(newNonce);
 </script>
 
 <template>

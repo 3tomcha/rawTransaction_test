916e78994cdd70bd914dc4b6f2c77ee3cbd21f85
Tomoya Kobayashi <tomocha.marika@gmail.com>
2022-03-06 12:52:57 +0900

    succeeded to send transaction

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,16 @@
 <script setup lang="ts">
-import Web3 from 'web3/dist/web3.min.js'
+import HomeView2 from "./HomeView2.vue";
+// import Web3 from 'web3/dist/web3.min.js'
 // import { Transaction } from "ethereumjs-tx";
-import WalletConnectProvider from "@walletconnect/web3-provider";
-const provider = new WalletConnectProvider({
-  infuraId: "674ff902cf5046cdb78c754e27467a6f"
-})
+// import WalletConnectProvider from "@walletconnect/web3-provider";
+// const provider = new WalletConnectProvider({
+//   infuraId: "674ff902cf5046cdb78c754e27467a6f"
+// })
 
-await provider.enable();
-// const provider = new Web3.providers.HttpProvider(`https://rinkeby.infura.io/v3/${import.meta.env.VITE_APP_INFURA_KEY}`);
-// const web3 = new Web3(provider);
+// await provider.enable();
+// const web3 = new Web3(String(provider));
 // const addressFrom = "0x32a9E70324862ef7BF8bA7610AF701822ddE5364";
 // const addressTo = "0x3D896D141dC4eEe51E829CcA7003939be20c280A";
-// const transactionCount = await web3.eth.getTransactionCount(addressFrom);
-// const newNonce = web3.utils.toHex(transactionCount);
 // const txData = {
 //   gasLimit: web3.utils.toHex(25000),
 //   gasPrice: web3.utils.toHex(10e9),
@@ -20,14 +18,27 @@ await provider.enable();
 //   from: addressFrom,
 //   value: web3.utils.toHex(web3.utils.toWei("123", "wei"))
 // }
-// const transaction = new Transaction({...txData, nonce: newNonce}, {chain: "ropsten"})
+// const transactionCount = await web3.eth.getTransactionCount(addressFrom);
+// const newNonce = web3.utils.toHex(transactionCount);
+
+// const send = () => {
+//   web3.eth.sendTransaction(txData).then((receipt) => {
+//     console.log(receipt);
+//   })
+// }
+
 // const privateKey = new Buffer(String(import.meta.env.PRIVATE_KEY), "hex");
 // transaction.sign(privateKey)
 // alert(newNonce);
+// const transaction = new Transaction({...txData, nonce: newNonce}, {chain: "ropsten"})
+// const res = await web3.eth.sendTransaction(txData);
+// console.log(res);
 </script>
 
 <template>
   <main>
-    aaa
+  <Suspense>
+    <HomeView2/>
+  </Suspense>
   </main>
 </template>
---
+++ src/views/HomeView2.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import Web3 from 'web3/dist/web3.min.js'
+import { Transaction } from "ethereumjs-tx";
+import WalletConnectProvider from "@walletconnect/web3-provider";
+const provider = new WalletConnectProvider({
+  infuraId: "674ff902cf5046cdb78c754e27467a6f"
+})
+
+await provider.enable();
+const web3 = new Web3(<any>provider);
+const addressTo = "0x3D896D141dC4eEe51E829CcA7003939be20c280A";
+
+const send = () => {
+    const txData = {
+    //   gasLimit: web3.utils.toHex(25000),
+    //   gasPrice: web3.utils.toHex(10e9),
+      to: addressTo,
+      from: "0x32a9E70324862ef7BF8bA7610AF701822ddE5364",
+      value: '1000000000000000'
+    //   value: web3.utils.toHex(web3.utils.toWei("123", "wei"))
+    }
+  web3.eth.sendTransaction(txData).then((receipt) => {
+    console.log(receipt);
+  })
+}
+
+const getAccount = async() => {
+    const accounts = await web3.eth.getAccounts();
+    if(accounts) {
+        console.log(accounts[0]);
+        return accounts[0];
+    }
+}
+
+// const transactionCount = await web3.eth.getTransactionCount(addressFrom).catch(err => {
+//     console.error(err);
+// });
+// const newNonce = web3.utils.toHex(Number(transactionCount));
+
+
+// const privateKey = new Buffer(String(import.meta.env.PRIVATE_KEY), "hex");
+// transaction.sign(privateKey)
+// alert(newNonce);
+// const transaction = new Transaction({...txData, nonce: newNonce}, {chain: "ropsten"})
+// const res = await web3.eth.sendTransaction(txData);
+// console.log(res);
+</script>
+
+
+<template>
+    <button @click="send">bbb</button>
+</template>
